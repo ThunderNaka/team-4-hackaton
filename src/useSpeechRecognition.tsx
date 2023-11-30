2b3f325a57910c346a345cb48c493d52f7aeb638
Naka <andresnakanishi@gmail.com>
2023-11-30 19:47:48 -0300

    Speech to text hook

---
+++ src/useSpeechRecognition.tsx
@@ -0,0 +1,46 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { useEffect, useState } from "react";
+
+let recognition: any = null;
+
+if ("webkitSpeechRecognition" in window) {
+  recognition = new webkitSpeechRecognition();
+  recognition.continuous = true;
+  recognition.lang = "en-US";
+}
+
+export const useSpeechRecognition = () => {
+  const [text, setText] = useState("");
+  const [isListening, setIsListening] = useState(false);
+
+  useEffect(() => {
+    if (!recognition) return;
+    recognition.onresult = (event: SpeechRecognitionEvent) => {
+      console.log("on result", event);
+      setText(event.results[0][0].transcript);
+      recognition.stop();
+      setIsListening(false);
+    };
+  }, []);
+
+  const startListening = () => {
+    setText("");
+    setIsListening(true);
+    recognition.start();
+  };
+
+  const stopListening = () => {
+    setIsListening(false);
+    recognition.stop();
+  };
+
+  return {
+    text,
+    isListening,
+    startListening,
+    stopListening,
+    hasRecognition: !!recognition,
+  };
+};
